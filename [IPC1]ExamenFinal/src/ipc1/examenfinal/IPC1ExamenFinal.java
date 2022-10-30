b3b56fee78bff5150fd399e6b9eed56b4f672b69
Ggi0 <gconcoha21@gmail.com>
2022-10-29 18:06:25 -0600

    prueba1

--- [IPC1]ExamenFinal/src/ipc1/examenfinal/IPC1ExamenFinal.java
+++ [IPC1]ExamenFinal/src/ipc1/examenfinal/IPC1ExamenFinal.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ipc1.examenfinal;
 
 /**
- *
- * @author Administrador
+ * Examen final - Introducción a la programación 1
+ * @author Giovanni Saul Concohá Cax
+ * carné: 202100229
  */
 public class IPC1ExamenFinal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mundo");
+        System.out.println("kkkk");
+        
     }
     
 }
